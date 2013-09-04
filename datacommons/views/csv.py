c8c44134da53a1d30ec053f12eb964c0752b7cd3
Matt Johnson <mdj2@pdx.edu>
2013-09-04 08:23:43 -0700

    Remove unused imports

--- datacommons/views/csv.py
+++ datacommons/views/csv.py
@@ -1,21 +1,5 @@
-import os
-import re
-import json
-from django.conf import settings as SETTINGS
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from django.db import DatabaseError
-from django.core.exceptions import PermissionDenied
-from django.contrib import messages
-from ..models.dbhelpers import (
-    getColumnsForTable,
-    getDatabaseTopology,
-)
-from ..models import ColumnTypes, ImportableUpload
 from ..forms.csvs import ImportableUploadForm, CSVPreviewForm
-from datacommons.jsonencoder import JSONEncoder
 from .importable import upload as upload_view, preview as preview_view
 
 @login_required
