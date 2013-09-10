a37a0fad9590bbdc1a0b84efc29628adf6f4dd0a
Matt Johnson <mdj2@pdx.edu>
2013-09-10 11:03:31 -0700

    Change paths in settings

--- datacommons/settings/base.py
+++ datacommons/settings/base.py
@@ -1,7 +1,7 @@
 import os
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
-PROJECT_DIR = os.path.dirname(__file__)
+PROJECT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), "../"))
 HOME_DIR = os.path.normpath(os.path.join(PROJECT_DIR, '../'))
 
 ALLOWED_HOSTS = ['.pdx.edu']
