1eeaaef6c58b10c5a5ea6131eff83ac11a3423df
Matt Johnson <mdj2@pdx.edu>
2013-09-12 12:32:31 -0700

    The readonly user should use the regular psycopg2 backend

--- datacommons/settings/example.py
+++ datacommons/settings/example.py
@@ -19,7 +19,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
     'readonly': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'datacommons_dev',                      # Or path to database file if using sqlite3.
         'USER': 'datacommons',                      # Not used with sqlite3.
         'PASSWORD': 'password',                  # Not used with sqlite3.
