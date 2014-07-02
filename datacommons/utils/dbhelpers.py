PSU-OIT-ARC/datacommons

commit 854f40956dd09b8718da8487a01237cd105995e7

Need to hide tables that weren't successfully uploaded (i.e. have a created_on field that is NULL)

datacommons/utils/dbhelpers.py
@@ -160,7 +160,8 @@ def getDatabaseTopology(owner=None):
         WHERE
             pg_namespace.nspowner != 10 AND
             nspname != 'geometries' AND
-            nspname NOT IN(%s, %s)
+            nspname NOT IN(%s, %s) AND
+            "t"."created_on" IS NOT NULL
         ORDER BY
             nspname, t.name, c.ordinal_position
     """

commit 6ecf4d70d4afd3179434588b55a114384340e8e4

Fix the bug introduced in 854f409 which hid every schema that didn't have a corresponding table object

datacommons/utils/dbhelpers.py
@@ -135,7 +135,7 @@ def getDatabaseTopology(owner=None):
             pg_namespace
         LEFT JOIN
             -- information_schema.tables t ON t.table_schema = nspname
-        "table" AS t ON t.schema = nspname 
+        "table" AS t ON t.schema = nspname AND t.created_on IS NOT NULL
         LEFT JOIN
             information_schema.columns c on c.table_schema = nspname AND t.name = c.table_name
         LEFT JOIN (
@@ -160,8 +160,7 @@ def getDatabaseTopology(owner=None):
         WHERE
             pg_namespace.nspowner != 10 AND
             nspname != 'geometries' AND
-            nspname NOT IN(%s, %s) AND
-            "t"."created_on" IS NOT NULL
+            nspname NOT IN(%s, %s)
         ORDER BY
             nspname, t.name, c.ordinal_position
     """